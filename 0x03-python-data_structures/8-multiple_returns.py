7862d30da578f1fb0a579249094b10e865464bb4
Jerry Nwadinobi <fernandojerry19911500@gmail.com>
2022-12-06 03:54:57 -0800

    added task 13

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -1,6 +1,18 @@
 #!/usr/bin/python3
+
+def multiple_returns(sentence):
+    if sentence != '':
+        first_char = sentence[0]
+    else:
+        first_char = None
+    return (len(sentence), first_char)
+
+
+'''
+#!/usr/bin/python3
 def multiple_returns(sentence):
     if len(sentence) == 0:
         return None
     else:
         return len(sentence), sentence[0]
+'''
