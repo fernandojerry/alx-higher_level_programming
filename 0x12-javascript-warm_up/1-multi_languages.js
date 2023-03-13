0c2d0d58ab47d9a1871bb3fb6f1d42b54cb0c5da
Jerry Nwadinobi <fernandojerry19911500@gmail.com>
2023-03-13 06:12:46 -0700

    added task 1

---
+++ 0x12-javascript-warm_up/1-multi_languages.js
@@ -0,0 +1,11 @@
+#!/usr/bin/node
+
+// A script that prints 3 lines
+
+const c = 'C is fun';
+const p = 'Python is cool';
+const j = 'JavScript is amazing';
+
+console.log(c);
+console.log(p);
+console.log(j);
